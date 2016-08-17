bc57b1c11b77b7f119dcda3f101d2d6009fd12c1
Rollie Ma <rollie.ma@gmail.com>
2016-08-17 08:15:35 -0700

    Fixing some typo in SQL

--- companies.sql
+++ companies.sql
@@ -14,7 +14,23 @@ INSERT INTO `companies` (`name`, `description`) VALUES
 ('Blue Cross', 'Blue Cross is an even better company.');
 
 -- For Question 2
--- 1. Add ndw column company_id to companies table,
--- 2. Add mew tabble countries, which will be consisted of two columns country_id and name.
--- 3. Add new table company_countries that will store one (company) to many (countries)
---    relations with two columns company_id and country_id.
+-- 1. Add new column `id` to `companies` table.
+-- CREATE TABLE IF NOT EXISTS `companies` (
+--   `id` int(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--   `name` varchar(100) NOT NULL,
+--   `description` text NOT NULL
+-- );
+--
+-- 2. Add mew table `states`, which will be consisted of two columns `id` and `name`.
+-- CREATE TABLE IF NOT EXISTS `states` (
+--   `id` int(2) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--   `name` varchar(50) NOT NULL
+-- );
+--
+-- 3. Add new table `company_states` that will store one (`company`) to many (`states`)
+--    relations with two columns `company_id` and `state_id`.
+-- CREATE TABLE IF NOT EXISTS `company_states` (
+--   `company_id` int(10) NOT NULL,
+--   `state_id` int(2) NOT NULL,
+--   CONSTRAINT `uc_company_state` UNIQUE (`company_id`, `state_id`)
+-- );
